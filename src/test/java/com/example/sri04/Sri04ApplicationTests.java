fab8176b3b34759d9ea785ec4ea0c28b26b80cea
Asia32000 <89982291+Asia32000@users.noreply.github.com>
2024-05-05 12:29:22 +0200

    add project

---
+++ src/test/java/com/example/sri04/Sri04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sri04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sri04ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
